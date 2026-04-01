10fabb6dadbb72098b9d245db4b83d1ee2d0462e
fuwangqin <fuwangqin@192.168.2.76>
2026-04-02 07:59:37 +0800

    feat: add ast module with Lua AST node definitions (22 nodes)

---
+++ ast/api/api.go
@@ -0,0 +1,326 @@
+// Package api defines the Lua Abstract Syntax Tree interface.
+// NO dependencies - pure interface definitions.
+package api
+
+import (
+	lexapi "github.com/akzj/go-lua/lex/api"
+	typesapi "github.com/akzj/go-lua/types/api"
+)
+
+// =============================================================================
+// Base Interfaces
+// =============================================================================
+
+// Node is the base interface for all AST nodes.
+// All nodes must implement Position() to support source mapping.
+type Node interface {
+	// Position returns the source position of this node.
+	// Why not embed Token? Some nodes (like blocks) span multiple tokens.
+	Position() (line int, column int)
+}
+
+// =============================================================================
+// Expression Nodes
+// =============================================================================
+
+// ExpNode is the base interface for all expression nodes.
+// Expressions produce values and can appear where values are expected.
+type ExpNode interface {
+	Node
+	// IsConstant returns true if this is a compile-time constant.
+	// Why not just check specific types? Parser needs uniform check.
+	IsConstant() bool
+}
+
+// =============================================================================
+// Expression Kinds (matching lua-master/lparser.h expkind enum)
+// =============================================================================
+
+// ExpKind represents the kind of an expression.
+// Why not just use interface type switches? Runtime type checks are expensive.
+// These constants allow fast comparison during code generation.
+type ExpKind int
+
+const (
+	EXP_VOID ExpKind = iota // VVOID - empty list marker
+	EXP_NIL                  // VNIL - constant nil
+	EXP_TRUE                 // VTRUE - constant true
+	EXP_FALSE                // VFALSE - constant false
+	EXP_K                    // VK - constant in constant table
+	EXP_KINTEGER             // VKINT - integer constant
+	EXP_KFLOAT               // VKFLT - float constant
+	EXP_KSTRING              // VKSTR - string constant
+	EXP_LOCAL                // VLOCAL - local variable
+	EXP_VARARG               // VVARGVAR - vararg parameter
+	EXP_GLOBAL               // VGLOBAL - global variable
+	EXP_UPVAL                // VUPVAL - upvalue variable
+	EXP_INDEXED              // VINDEXED - indexed variable
+	EXP_JMP                  // VJMP - test/comparison jump
+	EXP_RELOC                // VRELOC - relocatable expression
+	EXP_CALL                 // VCALL - function call
+)
+
+// ExpDesc describes a potentially delayed expression.
+// Similar to lua-master's expdesc structure.
+// Why separate from ExpNode? Runtime expression description needs
+// patch lists for short-circuit evaluation (and/or).
+type ExpDesc interface {
+	ExpNode
+	Kind() ExpKind
+	SetKind(ExpKind)
+	// Reg returns the register index (for VLOCAL, VNONRELOC, etc.)
+	Reg() int
+	SetReg(int)
+	// Info returns generic info field (constant index, etc.)
+	Info() int
+	SetInfo(int)
+	// Table returns table register and key register for VINDEXED.
+	Table() (tableReg int, keyReg int)
+	SetTable(tableReg, keyReg int)
+	// TrueJump returns the patch list for "exit when true".
+	TrueJump() int
+	SetTrueJump(int)
+	// FalseJump returns the patch list for "exit when false".
+	FalseJump() int
+	SetFalseJump(int)
+}
+
+// =============================================================================
+// Statement Nodes
+// =============================================================================
+
+// StatNode is the base interface for all statement nodes.
+// Statements are executable units that don't produce values.
+type StatNode interface {
+	Node
+	// IsScopeEnd returns true if this statement ends a scope.
+	// Used for local variable lifetime tracking.
+	IsScopeEnd() bool
+}
+
+// =============================================================================
+// Statement Kinds
+// =============================================================================
+
+// StatKind represents the kind of a statement.
+type StatKind int
+
+const (
+	STAT_ASSIGN StatKind = iota // Assignment statement
+	STAT_LOCAL_VAR               // Local variable declaration
+	STAT_LOCAL_FUNC              // Local function declaration
+	STAT_GLOBAL_FUNC             // Global function declaration
+	STAT_IF                      // If statement
+	STAT_WHILE                   // While statement
+	STAT_REPEAT                  // Repeat until statement
+	STAT_FOR_IN                  // For-in statement
+	STAT_FOR_NUM                 // Numeric for statement
+	STAT_RETURN                  // Return statement
+	STAT_BREAK                   // Break statement
+	STAT_GOTO                    // Goto statement
+	STAT_LABEL                   // Label statement
+	STAT_CALL                    // Expression statement (function call)
+	STAT_EMPTY                   // Empty statement (semicolon)
+)
+
+// =============================================================================
+// Variable Descriptor (matching lua-master Vardesc)
+// =============================================================================
+
+// VarDesc describes an active variable.
+type VarDesc interface {
+	// Name returns the variable name.
+	Name() string
+	// IsLocal returns true if this is a local variable.
+	IsLocal() bool
+	// IsGlobal returns true if this is a global variable.
+	IsGlobal() bool
+	// Reg returns the register holding this variable.
+	Reg() int
+	// Index returns the variable's index in actvar array.
+	Index() int
+}
+
+// =============================================================================
+// Label Descriptor (matching lua-master Labeldesc)
+// =============================================================================
+
+// LabelDesc describes a label statement.
+type LabelDesc interface {
+	// Name returns the label identifier.
+	Name() string
+	// Line returns the line where label appeared.
+	Line() int
+}
+
+// =============================================================================
+// Function Definition
+// =============================================================================
+
+// FuncDef represents a function definition.
+// Why separate from FunctionExp? FuncDef can appear in multiple contexts.
+type FuncDef interface {
+	// IsLocal returns true if this is a local function.
+	IsLocal() bool
+	// Line returns the line of the 'function' keyword.
+	Line() int
+	// LastLine returns the line of the 'end' keyword.
+	LastLine() int
+	// Proto returns the function prototype.
+	// Why return typesapi.Proto? Function body compilation produces Proto.
+	Proto() *typesapi.Proto
+}
+
+// =============================================================================
+// Table Constructor
+// =============================================================================
+
+// TableConstructor represents a table constructor expression.
+type TableConstructor interface {
+	ExpNode
+	// NumFields returns the number of array fields.
+	NumFields() int
+	// NumRecords returns the number of record fields.
+	NumRecords() int
+	// AddArrayField adds an array field.
+	AddArrayField(ExpNode)
+	// AddRecordField adds a record field (key, value).
+	AddRecordField(key, value ExpNode)
+}
+
+// =============================================================================
+// Function Call
+// =============================================================================
+
+// FuncCall represents a function call expression.
+type FuncCall interface {
+	ExpNode
+	// Func returns the function being called.
+	Func() ExpNode
+	// Args returns the call arguments.
+	Args() []ExpNode
+	// NumResults is the expected number of return values.
+	NumResults() int
+}
+
+// =============================================================================
+// Binary/Unary Operators
+// =============================================================================
+
+// BinopKind represents binary operators.
+type BinopKind int
+
+const (
+	BINOP_ADD BinopKind = iota //
+	BINOP_SUB                   //
+	BINOP_MUL                   //
+	BINOP_DIV                   //
+	BINOP_IDIV                  //
+	BINOP_MOD                   //
+	BINOP_POW                   //
+	BINOP_AND                   //
+	BINOP_OR                    //
+	BINOP_LT                    //
+	BINOP_GT                    //
+	BINOP_LE                    //
+	BINOP_GE                    //
+	BINOP_NE                    //
+	BINOP_EQ                    //
+	BINOP_SHL                   //
+	BINOP_SHR                   //
+	BINOP_CONCAT                //
+)
+
+// UnopKind represents unary operators.
+type UnopKind int
+
+const (
+	UNOP_NEG UnopKind = iota //
+	UNOP_NOT                  //
+	UNOP_BNOT                 //
+	UNOP_LEN                  //
+)
+
+// =============================================================================
+// Chunk / Block
+// =============================================================================
+
+// Chunk represents a complete Lua chunk (file or string).
+type Chunk interface {
+	// Block returns the chunk's block.
+	Block() *Block
+	// SourceName returns the source name.
+	SourceName() string
+}
+
+// Block represents a sequence of statements.
+type Block interface {
+	Node
+	// Stats returns all statements in this block.
+	Stats() []StatNode
+	// ReturnExp returns the return expression list (nil if no return).
+	ReturnExp() []ExpNode
+}
+
+// =============================================================================
+// Local Variable List (matching lua-master Dyndata.actvar)
+// =============================================================================
+
+// LocalVars maintains the list of active local variables.
+type LocalVars interface {
+	// Add adds a new local variable.
+	Add(name string, reg int)
+	// Get returns the variable at the given index.
+	Get(index int) VarDesc
+	// Count returns the number of active local variables.
+	Count() int
+	// Reset clears all variables.
+	Reset()
+}
+
+// =============================================================================
+// Goto/Label List (matching lua-master Labellist)
+// =============================================================================
+
+// GotoList maintains pending goto statements.
+type GotoList interface {
+	// Add adds a goto statement.
+	Add(name string, pc, line, nactvar int, close bool)
+	// Get returns the goto at the given index.
+	Get(index int) LabelDesc
+	// Count returns the number of pending gotos.
+	Count() int
+}
+
+// LabelList maintains active labels.
+type LabelList interface {
+	// Add adds a label.
+	Add(name string, pc, line, nactvar int)
+	// Get returns the label at the given index.
+	Get(index int) LabelDesc
+	// Count returns the number of active labels.
+	Count() int
+}
+
+// =============================================================================
+// Parser Interface
+// =============================================================================
+
+// Parser parses Lua source code into an AST.
+type Parser interface {
+	// ParseChunk parses a complete Lua chunk.
+	ParseChunk(source string) (Chunk, error)
+	// ParseExpression parses a single expression.
+	// Used for -e flag and loadstring.
+	ParseExpression(source string) (ExpNode, error)
+}
+
+// =============================================================================
+// Token Helper
+// =============================================================================
+
+// HasToken provides token access for position reporting.
+type HasToken interface {
+	// Token returns the first token of this node.
+	Token() lexapi.Token
+}
---
+++ ast/internal/ast_test.go
@@ -0,0 +1,324 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/akzj/go-lua/ast/api"
+)
+
+func TestExpKind(t *testing.T) {
+	// Test that exp kinds are in expected order
+	if api.EXP_NIL != 1 {
+		t.Errorf("EXP_NIL should be 1, got %d", api.EXP_NIL)
+	}
+}
+
+func TestStatKind(t *testing.T) {
+	// Test that stat kinds are in expected order
+	if api.STAT_ASSIGN != 0 {
+		t.Errorf("STAT_ASSIGN should be 0, got %d", api.STAT_ASSIGN)
+	}
+}
+
+func TestBinopKind(t *testing.T) {
+	// Test that binop kinds are in expected order
+	if api.BINOP_ADD != 0 {
+		t.Errorf("BINOP_ADD should be 0, got %d", api.BINOP_ADD)
+	}
+	if api.BINOP_CONCAT != 17 {
+		t.Errorf("BINOP_CONCAT should be 17, got %d", api.BINOP_CONCAT)
+	}
+}
+
+func TestUnopKind(t *testing.T) {
+	if api.UNOP_NEG != 0 {
+		t.Errorf("UNOP_NEG should be 0, got %d", api.UNOP_NEG)
+	}
+}
+
+func TestNilExp(t *testing.T) {
+	exp := &NilExp{}
+	exp.Line, exp.Column = 1, 5
+
+	if !exp.IsConstant() {
+		t.Error("NilExp should be constant")
+	}
+
+	line, col := exp.Position()
+	if line != 1 || col != 5 {
+		t.Errorf("Position() = (%d, %d), want (1, 5)", line, col)
+	}
+}
+
+func TestTrueExp(t *testing.T) {
+	exp := &TrueExp{}
+	exp.Line = 1
+
+	if !exp.IsConstant() {
+		t.Error("TrueExp should be constant")
+	}
+}
+
+func TestFalseExp(t *testing.T) {
+	exp := &FalseExp{}
+
+	if !exp.IsConstant() {
+		t.Error("FalseExp should be constant")
+	}
+}
+
+func TestIntegerExp(t *testing.T) {
+	exp := &IntegerExp{Value: 42}
+
+	if !exp.IsConstant() {
+		t.Error("IntegerExp should be constant")
+	}
+}
+
+func TestFloatExp(t *testing.T) {
+	exp := &FloatExp{Value: 3.14}
+
+	if !exp.IsConstant() {
+		t.Error("FloatExp should be constant")
+	}
+}
+
+func TestStringExp(t *testing.T) {
+	exp := &StringExp{Value: "hello"}
+
+	if !exp.IsConstant() {
+		t.Error("StringExp should be constant")
+	}
+}
+
+func TestVarargExp(t *testing.T) {
+	exp := &VarargExp{}
+
+	if exp.IsConstant() {
+		t.Error("VarargExp should not be constant")
+	}
+}
+
+func TestNameExp(t *testing.T) {
+	exp := &NameExp{Name: "x"}
+
+	if exp.IsConstant() {
+		t.Error("NameExp should not be constant")
+	}
+}
+
+func TestExpDescImpl(t *testing.T) {
+	desc := &ExpDescImpl{
+		Kind_:    api.EXP_LOCAL,
+		Reg_:     5,
+		Info_:    10,
+	}
+
+	if desc.Kind() != api.EXP_LOCAL {
+		t.Errorf("Kind() = %d, want %d", desc.Kind(), api.EXP_LOCAL)
+	}
+
+	if desc.Reg() != 5 {
+		t.Errorf("Reg() = %d, want 5", desc.Reg())
+	}
+
+	if desc.Info() != 10 {
+		t.Errorf("Info() = %d, want 10", desc.Info())
+	}
+
+	desc.SetKind(api.EXP_GLOBAL)
+	if desc.Kind() != api.EXP_GLOBAL {
+		t.Error("SetKind failed")
+	}
+
+	desc.SetReg(8)
+	if desc.Reg() != 8 {
+		t.Error("SetReg failed")
+	}
+
+	desc.SetTable(3, 4)
+	tReg, kReg := desc.Table()
+	if tReg != 3 || kReg != 4 {
+		t.Errorf("Table() = (%d, %d), want (3, 4)", tReg, kReg)
+	}
+
+	desc.SetTrueJump(100)
+	if desc.TrueJump() != 100 {
+		t.Error("SetTrueJump failed")
+	}
+
+	desc.SetFalseJump(200)
+	if desc.FalseJump() != 200 {
+		t.Error("SetFalseJump failed")
+	}
+}
+
+func TestBinopExp(t *testing.T) {
+	left := &IntegerExp{Value: 1}
+	right := &IntegerExp{Value: 2}
+	exp := &BinopExp{Op: api.BINOP_ADD, Left: left, Right: right}
+
+	if !exp.IsConstant() {
+		t.Error("BinopExp with constant operands should be constant")
+	}
+}
+
+func TestUnopExp(t *testing.T) {
+	sub := &IntegerExp{Value: -5}
+	exp := &UnopExp{Op: api.UNOP_NEG, Exp: sub}
+
+	if !exp.IsConstant() {
+		t.Error("UnopExp with constant operand should be constant")
+	}
+}
+
+func TestTableConstructorImpl(t *testing.T) {
+	tc := &TableConstructorImpl{}
+	tc.AddArrayField(&IntegerExp{Value: 1})
+	tc.AddArrayField(&IntegerExp{Value: 2})
+	tc.AddRecordField(&StringExp{Value: "key"}, &IntegerExp{Value: 3})
+
+	if tc.NumFields() != 2 {
+		t.Errorf("NumFields() = %d, want 2", tc.NumFields())
+	}
+
+	if tc.NumRecords() != 1 {
+		t.Errorf("NumRecords() = %d, want 1", tc.NumRecords())
+	}
+
+	if !tc.IsConstant() {
+		t.Error("TableConstructorImpl with constant fields should be constant")
+	}
+}
+
+func TestFuncCallImpl(t *testing.T) {
+	fc := &FuncCallImpl{
+		Func_:       &NameExp{Name: "print"},
+		Args_:       []api.ExpNode{&StringExp{Value: "hello"}},
+		NumResults_: 0,
+	}
+
+	if fc.Func() == nil {
+		t.Error("Func() should not be nil")
+	}
+
+	if len(fc.Args()) != 1 {
+		t.Errorf("Args() length = %d, want 1", len(fc.Args()))
+	}
+
+	if fc.NumResults() != 0 {
+		t.Error("NumResults() should be 0")
+	}
+}
+
+func TestFuncDefImpl(t *testing.T) {
+	fd := &FuncDefImpl{
+		Line_:    1,
+		LastLine_: 10,
+		IsLocal_: true,
+	}
+
+	if !fd.IsLocal() {
+		t.Error("IsLocal() should be true")
+	}
+
+	if fd.Line() != 1 {
+		t.Error("Line() should be 1")
+	}
+
+	if fd.LastLine() != 10 {
+		t.Error("LastLine() should be 10")
+	}
+}
+
+func TestBlockImpl(t *testing.T) {
+	block := &BlockImpl{
+		Stats_:     []api.StatNode{&ReturnStat{}},
+		ReturnExp_: []api.ExpNode{&IntegerExp{Value: 42}},
+	}
+
+	if len(block.Stats()) != 1 {
+		t.Errorf("Stats() length = %d, want 1", len(block.Stats()))
+	}
+
+	if len(block.ReturnExp()) != 1 {
+		t.Errorf("ReturnExp() length = %d, want 1", len(block.ReturnExp()))
+	}
+}
+
+func TestChunkImpl(t *testing.T) {
+	chunk := &ChunkImpl{
+		Block_:      &BlockImpl{},
+		SourceName_: "=(stdin)",
+	}
+
+	if chunk.SourceName() != "=(stdin)" {
+		t.Error("SourceName() should be '=(stdin)'")
+	}
+}
+
+func TestVarDescImpl(t *testing.T) {
+	vd := &VarDescImpl{
+		Name_:    "x",
+		IsLocal_: true,
+		Reg_:     5,
+		Index_:   0,
+	}
+
+	if vd.Name() != "x" {
+		t.Error("Name() should be 'x'")
+	}
+
+	if !vd.IsLocal() {
+		t.Error("IsLocal() should be true")
+	}
+
+	if vd.IsGlobal() {
+		t.Error("IsGlobal() should be false")
+	}
+
+	if vd.Reg() != 5 {
+		t.Error("Reg() should be 5")
+	}
+}
+
+func TestLocalVarsImpl(t *testing.T) {
+	lv := &LocalVarsImpl{}
+	lv.Add("a", 0)
+	lv.Add("b", 1)
+
+	if lv.Count() != 2 {
+		t.Errorf("Count() = %d, want 2", lv.Count())
+	}
+
+	v0 := lv.Get(0)
+	if v0 == nil || v0.Name() != "a" {
+		t.Error("Get(0) should return 'a'")
+	}
+
+	lv.Reset()
+	if lv.Count() != 0 {
+		t.Error("After Reset(), Count() should be 0")
+	}
+}
+
+func TestAssignStat(t *testing.T) {
+	stat := &AssignStat{}
+	if stat.IsScopeEnd() {
+		t.Error("AssignStat should not end scope")
+	}
+}
+
+func TestReturnStat(t *testing.T) {
+	stat := &ReturnStat{}
+	if !stat.IsScopeEnd() {
+		t.Error("ReturnStat should end scope")
+	}
+}
+
+func TestBreakStat(t *testing.T) {
+	stat := &BreakStat{}
+	if !stat.IsScopeEnd() {
+		t.Error("BreakStat should end scope")
+	}
+}
